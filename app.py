d03588e8984e3a683f51f781c7ba317900742797
Rohan Datta <rohan.datta314@gmail.com>
2023-04-14 00:54:49 +0530

    Added code for the image classifier

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:279c3cfe35cc7c5a387c00baa9e15f05c36781dacb327a9b7607ea07c245967f
-size 147
+oid sha256:f055bb3086f632b5e55f80d9c14b21501c2d4c397ef263ba3e27e54451b87144
+size 1326
