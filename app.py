daa825e99257ff19cedbf17e68e6044f2cc67728
Rohan Datta <rohan.datta314@gmail.com>
2023-04-17 01:07:48 +0530

    Repointed fastbook functions to remove dependency

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b9577c25cad75819b9a1166e11bdd03baae2c78cb24ddcc2c62dc1698c54bc6
-size 1633
+oid sha256:f7ad65090ae9aabeb0dee11e66f4f3dabf6683ba7272e18aebefa3eef422ccce
+size 1675
